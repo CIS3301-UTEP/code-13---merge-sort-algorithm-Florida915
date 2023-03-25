cfdb62352ad504ac748dbb318471454f4b4b6d16
Florida915 <123209134+Florida915@users.noreply.github.com>
2023-03-24 22:36:19 -0600

    Update code_13.py

--- code_13.py
+++ code_13.py
@@ -1,5 +1,18 @@
 def get_merge_sorted_list(unsorted_list):
-    pass #Remove this line of code and implement your merge sort algorithm
+    if len(unsorted_list)==1:
+        return unsorted_list
+
+unsorted_list= [2,3,4,5,6,1,7]
+mid_point = int((len(unsorted_list))//2)
+
+first_part = unsorted_list[:mid_point]
+second_part =unsorted_list[mid_point:]
+
+half_a = get_merge_sorted_list(first_part)
+half_b = get_merge_sorted_list(second_part)
+
+    
 
 if __name__ == "__main__":
-    pass #Remove this line and program your function calls to locally test the functionallity of merge sort
+
+    print(get_merge_sorted_list(unsorted_list))
